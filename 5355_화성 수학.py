0c66099c15911f7bf21f0493fe5dbec9476b29c6
ta-ho <tigerho98@hanyang.ac.kr>
2023-11-23 17:44:40 +0900

    문제집: Python 배우기 15/50

--- 5355_화성 수학.py
+++ 5355_화성 수학.py
@@ -25,12 +25,12 @@ result = []
 num_cases = int(input())
 for i in range(num_cases):
     input_cal.append(list(input().split()))
-    for j in range(len(input_cal[i])-1):
-        cal = float(input_cal[i][0])
-
-        if input_cal[i][j+1] == '@':
+    cal = float(input_cal[i][0]) # 이번에도 이놈을 j for문 안으로 넣어 답이 안 나와 헤맸다. ** 계속 계산되는 변수 초기값은 반복문 밖으로 **
+    
+    for j in range(1, len(input_cal[i])):
+        if input_cal[i][j] == '@':
             cal *= 3
-        elif input_cal[i][j+1] == '%':
+        elif input_cal[i][j] == '%':
             cal += 5
         else:
             cal -= 7
