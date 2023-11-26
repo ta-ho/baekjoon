dcffd50911870edc60b73771ae857bc88e43208c
ta-ho <150849738+ta-ho@users.noreply.github.com>
2023-11-26 21:44:34 +0900

    [Bronze III] Title: 직각삼각형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    a,b,c = map(int, input().split())
+    if a==0 and b==0 and c==0:
+        break
+    elif (a**2 + b**2 == c**2) or (b**2+c**2==a**2) or(c**2+a**2==b**2):
+        print("right")
+    else:
+         print("wrong")
