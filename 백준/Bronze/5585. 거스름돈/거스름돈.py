d01516c98c80042e1c2b7238897c3a45f7e4310d
ta-ho <150849738+ta-ho@users.noreply.github.com>
2023-11-27 00:29:28 +0900

    [Bronze II] Title: 거스름돈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -3,7 +3,7 @@ array = [500, 100, 50, 10, 5, 1]
 change = 1000-int(input())
 num = 0
 for i in array:
-    num += change // i
+    num += change // i # 해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
     change %= i
 
 print(num)
